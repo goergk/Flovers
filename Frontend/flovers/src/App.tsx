81d224aa345d9df2ab9955ed427a68ed86cbd078
goergk <grzegorzkaw124@gmail.com>
2021-12-21 14:40:33 +0100

    React project created & cleaned files

---
+++ Frontend/flovers/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Initial commit
+      </p>
+    </div>
+  );
+}
+
+export default App;

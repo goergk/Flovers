c58ef7c870012861e48c2755c704e53038a1ba4e
goergk <grzegorzkaw124@gmail.com>
2022-02-17 12:57:46 +0100

    Added Loader component, changed backgrounds in Deliveries & Resources

---
+++ Frontend/flovers/src/components/Assets/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoaderSvg from './Loader.svg';
+
+const Loader = () => {
+    return (
+        <img src={LoaderSvg} alt="Loader" />
+    )
+}
+
+export default Loader;

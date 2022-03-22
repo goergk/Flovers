5ec46967f62e8dfb57c1108a258b46098292bf3d
goergk <grzegorzkaw124@gmail.com>
2022-03-22 23:01:55 +0100

    Setup for installing Django dependencies

--- Backend/flovers/flovers/settings.py
+++ Backend/flovers/flovers/settings.py
@@ -137,7 +137,7 @@ REST_FRAMEWORK = {
     }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=360),
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
